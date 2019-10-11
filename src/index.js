295f156e527d95a9550c3b39c8fdf97f6bee13f9
Justin Konecny <konecnyjustin@gmail.com>
2019-10-11 18:12:51 -0400

    Remove firebase files

--- src/index.js
+++ src/index.js
@@ -4,19 +4,11 @@ import * as serviceWorker from './serviceWorker';
 import { BrowserRouter } from 'react-router-dom';
 import App from './components/App';
 import './index.css';
-
-const config = {
-    apiKey: "AIzaSyB6qyO-H0iQjx1zZzPodJLr3PB-4GFFhRs",
-    authDomain: "calendays-ccfc4.firebaseapp.com",
-    databaseURL: "https://calendays-ccfc4.firebaseio.com",
-    projectId: "calendays-ccfc4",
-    storageBucket: "calendays-ccfc4.appspot.com",
-    messagingSenderId: "634670053644"
-};
+import { Config } from './config';
 
 const firebase = require('firebase/app');
 require('firebase/auth');
-firebase.initializeApp(config);
+firebase.initializeApp(Config);
 
 const routerApp = (
     <BrowserRouter>
